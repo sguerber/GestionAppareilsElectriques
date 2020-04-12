4291b256cee41bca8cda207dc73289b65e23e5cc
sguerber <g.sarah.jobs@outlook.fr>
2020-04-12 14:21:32 +0200

    Commit pour l'ajout du service et du routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AppareilService } from './services/appareil.service';
 
 @Component({
   selector: 'app-root',
@@ -8,44 +7,18 @@ import { AppareilService } from './services/appareil.service';
 })
 export class AppComponent implements OnInit{
 	
-
-  isAuth=false;
-  
-  appareils: any[];
-	
-  constructor(private appareilService: AppareilService) {
-    setTimeout(
-      () => {
-        this.isAuth = true;
-      }, 4000
-    );
+		
+  constructor() {
   }
   
   ngOnInit() {
-    this.appareils = this.appareilService.appareils;
+    
 } 
+
   
-  // simulation de recherche d'une donnée sur un serveur
-  // -> on crée une Promise disponible seulement au bout de deux secondes
-  lastUpdate = new Promise((resolve, reject) => {
-    const date = new Date();
-    setTimeout(
-      () => {
-        resolve(date);
-      }, 2000
-    );
-  })
   
-  onAllumer() {
-    this.appareilService.switchOnAll();
-	}
-  onEteindre() {
-    if(confirm('Etes-vous sûr de vouloir éteindre tous vos appareils ?')) {
-      this.appareilService.switchOffAll();
-    } else {
-      return null;
-    }
-  }
+  
+  
 
   
   
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,27 @@ import { AppComponent } from './app.component';
 import { AppareilComponent } from './appareil/appareil.component';
 import { FormsModule } from '@angular/forms';
 import { AppareilService } from './services/appareil.service';
+import { AuthComponent } from './auth/auth.component';
+import { AppareilViewComponent } from './appareil-view/appareil-view.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+	{ path: 'appareils', component: AppareilViewComponent},
+	{ path: 'auth', component: AuthComponent},
+	{ path: '', component: AppareilViewComponent}
+]; 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppareilComponent
+    AppareilComponent,
+    AuthComponent,
+    AppareilViewComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+	RouterModule.forRoot(appRoutes)
   ],
   providers: [
   AppareilService
